1fd1e7aec745290aeb8875b2962fe1385d87e295
liamlowey <liamlowey@684a87ce-2fe7-11de-ba90-e1c2dc4d389a>
2009-08-13 15:31:17 +0000

    Beginnings of a live timing viewer application. Nothing usable yet though.

---
+++ AppCore/LiveTimingDataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using AppCore.Schemas;
+
+namespace AppCore
+{
+    public class LiveTimingDataProvider
+    {
+        private readonly MainStateDS _data;
+
+        public LiveTimingDataProvider()
+        {
+            _data = new MainStateDS();
+
+            for (int i = 0; i < 20; i++)
+            {
+                _data.Main.AddMainRow(i, i, "Driver " + (i + 1), Math.Round(Math.Sin(i), 2), i * 0.5, 2.33 + Math.Round(Math.Cos(i),2 ), 22 + Math.Round(Math.Sin(i),2),
+                                   23 + Math.Round(Math.Cos(i),2), 24 + Math.Round(Math.Sin(i),2), 1);
+            }
+
+            _data.AcceptChanges();
+        }
+
+
+        public DataView DefaultView
+        {
+            get
+            {
+                return _data.Main.DefaultView;
+            }
+        }
+    }
+}
---
+++ Live Timing Viewer/TimingGrid.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Live_Timing_Viewer
+{
+	/// <summary>
+	/// Interaction logic for TimingGrid.xaml
+	/// </summary>
+	public partial class TimingGrid : UserControl
+	{
+		public TimingGrid()
+		{
+            InitializeComponent();
+        }
+	}
+}

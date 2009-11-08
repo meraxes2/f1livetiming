4f9cdffe60c5e236a5b2a4999a26332d62ea6480
liamlowey <liamlowey@684a87ce-2fe7-11de-ba90-e1c2dc4d389a>
2009-11-08 19:24:25 +0000

    More XAML stuff

---
+++ Live Timing Viewer/F1LiveLogo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Live_Timing_Viewer
+{
+	/// <summary>
+	/// Interaction logic for F1LiveLogo.xaml
+	/// </summary>
+	public partial class F1LiveLogo : UserControl
+	{
+		public F1LiveLogo()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
